c26975a1d35494b8586708de16b22a9175fa1daf
Shakilrev <134630735+Shakilrev@users.noreply.github.com>
2023-10-31 23:53:07 +0600

    Add files via upload

---
+++ if_condition.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    if(1)
+    {
+        cout<<"true";
+    }
+    else{
+        cout<<"false";
+    }
+    cout<<endl;
+    int roll=1;
+    if(roll<2)
+    {
+        cout<<"well done";
+    }
+    else{
+        cout<<"work hard";
+    }
+}
